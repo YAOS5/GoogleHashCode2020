e811b3d9ebd22be2cede0d999c51349aad4d353b
Shivam Agarwal <shivam2003agarwal@gmail.com>
2020-02-15 22:15:03 +1100

    Update input

--- Input.java
+++ Input.java
@@ -3,31 +3,37 @@ package com.company;
 import java.io.File;  // Import the File class
 import java.util.*;
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.stream.Collectors;
 
 public class Input {
 
-    ArrayList <Integer> slices = new ArrayList<Integer>();
-    int maxSlices;
+    ArrayList <String> slices;
+    String maxSlices;
 
-    public void readTextFile() thows FileNotFoundException {
-      FileReader inputStream = null;
+    public void readTextFile() {
       try {
-        inputStream = new FileReader("c_medium.txt");
-        int c;
-      while((c = inputStream.read()) != -1) {
-        if(flag == 2) break;
-        if()
+        Scanner sc = null;
+        sc = new Scanner(new File("/Users/shivam/Documents/GitHub/GoogleHashCode20/src/com/company/data/b_small.in"));
+        String line1 = sc.nextLine();
+        String[] words1 = line1.split(" ");
+        String line2 = sc.nextLine();
+        String[] words2 = line2.split(" ");
+        maxSlices = words1[0];
+        slices = new ArrayList<String>(Arrays.asList(words2));
       }
-      myReader.close();
+      catch (FileNotFoundException e) {
+        e.printStackTrace();
       }
+
     }
 
+
     public ArrayList<Integer> getNoPizzaSlices() {
-      return slices;
+      return slices.stream().map(Integer::valueOf).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public int getMaxSlices() {
-      return maxSlices;
+      return Integer.valueOf(maxSlices);
     }
 
 }
