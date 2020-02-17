0a1063e207f3f2c34d4fb7be34dc8f810735fb86
Uvin <donuvin@gmail.com>
2020-02-17 21:00:07 +1100

    Finished combinations.

--- combinations.java
+++ combinations.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class combinations {
+
+    public static ArrayList<PQObject> createCombinations(ArrayList<Server> servers,  ArrayList<Video> videos, ArrayList<EndPoint> endpoints ) {
+
+        ArrayList<PQObject> output = new ArrayList<PQObject>();
+
+        for (Server server : servers){
+            for (Video video : videos ){
+                output.add( new PQObject(server, video, endpoints));
+            }
+        }
+        
+        return output;
+
+    }
+
+
 }
