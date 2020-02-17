d687756a141e118b93069fc8c377c0b788a83343
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-02-17 20:07:12 +1100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,10 +5,5 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	    Input input = new Input();
-	    input.readTextFiles();
-        ArrayList<Integer> result = DummyAlgo2(input.getMaxPizzaSlices, input.getNoPizzaSlices);
-
-        output.Tofile(result);
     }
 }
