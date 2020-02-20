419052e0e949c788ae186514affcd42b7903bea7
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-02-21 07:16:39 +1100

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,13 +1,17 @@
-class Book {
+class Book implements Comparable<Book> {
   int id;
   int score;
   BookState state;
 
-  public Book(int id, int score) {
+  public Book(int id, int score){
     this.id = id;
     this.score = score;
     state = BookState.UNSCANNED;
   }
 
-
+  public int compareTo(Book b)
+      {
+        // need the books with the smallest score to come first
+        return this.score - b.score;
+      }
 }
