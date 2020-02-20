567e29e23728820a4dbf6f054eaf0f88a8715d40
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-02-21 05:47:15 +1100

    Loaded the datasets

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+      Input.read();
+      GoogleBooks googleBooks = Input.getGoogleBooks();
 
+      ArrayList<Library> libraries = googleBooks.operate();
+      Output.outputToFile(libraries);
     }
 }
