8fd2a71336bbf9e81448506e047601aba7c6f79b
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-02-21 06:01:51 +1100

    Completed the scan function

--- Library.java
+++ Library.java
@@ -7,19 +7,37 @@ class Library {
   int signUpDays;
   int scanPerDay;
   boolean isSignedUp;
-  HashSet<Book> books;
+  ArrayList<Book> booksLeft;
   ArrayList<Book> scannedBooks;
 
 
-  public Library(int id, int signUpDays, int scanPerDay, HashSet<Book> books) {
+  public Library(int id, int signUpDays, int scanPerDay, ArrayList<Book> booksLeft) {
     this.id = id;
     this.signUpDays = signUpDays;
     this.scanPerDay = scanPerDay;
-    this.books = books;
+    this.booksLeft = booksLeft;
 
     this.isSignedUp = false;
     this.scannedBooks = new ArrayList<>();
     this.state = LibraryState.NOT_SIGNED_UP;
+  }
+
+
+  public void scan() {
+    if (state == LibraryState.COMPLETE)
+      return;
+
+    this.state = LibraryState.SCANNING_BOOKS;
+    // scan as many books as I can for now
+    int booksToScan = Math.min(scanPerDay, booksLeft.size());
+    for (int i = 0; i < booksToScan; i++) {
+        Book book = booksLeft.remove(booksLeft.size() - 1);
+        book.state = BookState.SCANNED;
+        scannedBooks.add(book);
+    }
+
+    if (booksLeft.size() == 0)
+      this.state = LibraryState.COMPLETE;
   }
 
 
